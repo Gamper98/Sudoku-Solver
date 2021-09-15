653ec3faa0b35060718e7f4c2778276fd9446313
Gamper98 <gampi1998@gmail.com>
2021-09-15 11:36:22 +0200

    Refactored, Added features
    Refactored it so would be more readable, moved everything to separate methods, and put all of it in a class
    
    Added to show which sqrs contain and can contain a selected sqr number
    
    Changes so not Buttons, but Graphs represent the board
    
    Removed locked sqrs

--- Sudoku_Window.py
+++ Sudoku_Window.py
@@ -1,136 +1,194 @@
 import PySimpleGUI as sg
-from PySimpleGUI.PySimpleGUI import SELECT_MODE_SINGLE
 from Sudoku_Modell import *
 
-def set_history(window, model):
-    his = model.get_board().get_history()
-    his_len = model.get_board().get_current_his_pos()
-    window['history'].update(his, set_to_index=[his_len])
+class Sudoku_Window():
+    def __init__(self):
+        self.__active_sq = None
+        self.__model = Sudoku_Modell()
+        self.__window = sg.Window("Sudoku megoldó", self.__get_layout(), return_keyboard_events=True).Finalize()
+        self.__containing_sqs = []
+        self.__designs = {
+            'inactive_color' : '#283b5b',
+            'active_color' : '#ABABAB',
+            'containing_sqs' : '#7E6E5A',
+        }
 
-def update_board(window, model):
-    for row in range(9):
-        for col in range(9):
-            if model.get_board().get_board()[row, col] != 0:
-                window[(row, col)].update(model.get_board().get_board()[row, col])
-            else: window[(row, col)].update('')
+    def __get_layout(self):
+        p_name = self.__model.get_pattern_names()
+        pattern_checkbox_list = [[sg.Text('Használni kivánt minták:')]] +\
+                [[sg.Checkbox('{}'.format(p_name[i]), default=True, k=i+100)]
+                for i in range(len(p_name))]
 
-def get_layout(model):
-    p_name = model.get_solver().get_patterns_name()
-    pattern_checkbox_list = [[sg.Text('Használni kivánt minták:')]] +\
-            [[sg.Checkbox('{}'.format(p_name[i]), default=True, k=i+10)]
-            for i in range(len(p_name))]
+        sudoku_input_grid = [[sg.Graph(
+                                (36,36),
+                                (0,0),
+                                (36,36),
+                                pad=(((1,int(col%3==2)*3+1),(1,int(row%3==2)*3+1))),
+                                background_color='#283b5b',
+                                k=(row,col),
+                                enable_events=True)                        
+                    for col in range(9)] 
+                        for row in range(9)] +\
+            [[
+            sg.Button('Clear', k='clear'),
+            sg.Button('Solve', k='solve'),
+            sg.Button('Fill pencil marks', k='pen_marks')
+            ]]
 
-    sudoku_input_grid = [[sg.Button(
-                            size=(2,1), 
-                            pad=((0,int(col%3==2)*3),(0,int(row%3==2)*3)),
-                            k=(row,col),
-                            disabled=False,
-                            enable_events=True)                        
-                for col in range(9)] 
-                    for row in range(9)] +\
-        [[sg.Button(i+1, k=i+1) for i in range(9)]] +\
-        [[
-        sg.Button('Clear', k='clear'),
-        #sg.Button('Lock', k='lock'),
-        #sg.Button('Unlock', k='unlock'),
-        sg.Button('Solve', k='solve')
-        ]]
+        file_load_and_choose_list = [
+            [
+                sg.Text('Sudoku fájl betöltés')
+            ],
+            [
+                sg.In(size=(25, 1), k='folder', readonly=True, disabled=True, enable_events=True),
+                sg.FilesBrowse(file_types=(('Json Files', '*.json'),('All Files', '*.*'),))
+            ],
+            [
+                sg.Listbox(values=[], enable_events=True, size =(31,15), k='sudoku_list', select_mode=sg.SELECT_MODE_SINGLE)
+            ]]
 
-    file_load_and_choose_list = [
-        [
-            sg.Text('Sudoku fájl betöltés')
-        ],
-        [
-            sg.In(size=(25, 1), k='folder', readonly=True, disabled=True, enable_events=True),
-            sg.FilesBrowse(file_types=(('Json Files', '*.json'),('All Files', '*.*'),))
-        ],
-        [
-            sg.Listbox(values=[], enable_events=True, size =(31,15), k='sudoku_list', select_mode=SELECT_MODE_SINGLE, )
-        ]]
+        history_column = [
+            [
+                sg.Text('Eddigi lépések')
+            ],
+            [
+                sg.Listbox(values=[], enable_events=False, size=(15,15), k='history', select_mode=sg.SELECT_MODE_SINGLE)
+            ],
+            [
+                sg.Button('Visszalép', k='back'),
+                sg.Button('Előrelép', k='forward')
+            ]]
 
-    history_column = [
-        [
-            sg.Text('Eddigi lépések')
-        ],
-        [
-            sg.Listbox(values=[], enable_events=False, size=(15,15), k='history', select_mode=SELECT_MODE_SINGLE)
-        ],
-        [
-            sg.Button('Visszalép', k='back'),
-            sg.Button('Előrelép', k='forward')
-        ]]
+        layout = [[
+            sg.Column(pattern_checkbox_list),
+            sg.VSeperator(k='s1'),
+            sg.Column(sudoku_input_grid, element_justification='center'),
+            sg.VSeperator(k='s2'),
+            sg.Column(file_load_and_choose_list),
+            sg.VSeperator(k='s3'),
+            sg.Column(history_column)
+            ]]
 
-    layout = [[
-        sg.Column(pattern_checkbox_list),
-        sg.VSeperator(k='vs1'),
-        sg.Column(sudoku_input_grid, element_justification='center'),
-        sg.VSeperator(k='vs2'),
-        sg.Column(file_load_and_choose_list),
-        sg.VSeperator(k='vs3'),
-        sg.Column(history_column)
-        ]]
+        return layout
 
-    return layout
+    def __set_active_sq(self, event):
+        if self.__active_sq is not None:
+            self.__window[self.__active_sq].update(self.__designs['inactive_color'])
+            for pos in self.__containing_sqs:
+                self.__window[tuple(pos)].update(self.__designs['inactive_color'])
+            self.__containing_sqs = []
+        self.__window[event].update(self.__designs['active_color'])
+        self.__active_sq = event
 
-model = Sudoku_Modell()
-window = sg.Window("Sudoku megoldó", get_layout(model))
+        num = self.__model.get_board_at(event)
+        if num == 0:
+            return
+        sqrs = np.argwhere(self.__model.get_board() == num)
+        pv_sqrs = np.argwhere(self.__model.get_pv_at(num-1))
+        self.__containing_sqs = np.concatenate((sqrs, pv_sqrs))
+        for pos in self.__containing_sqs:
+            if tuple(pos) == self.__active_sq: continue
+            self.__window[tuple(pos)].update(self.__designs['containing_sqs'])
 
-active_sudoku_sqare = None
-while True:
-    event, values = window.read()
-    if event == "Exit" or event == sg.WIN_CLOSED:
-        break
-    if isinstance(event, tuple):
-        if active_sudoku_sqare is not None:
-            window[active_sudoku_sqare].update(button_color='#283b5b')
-        active_sudoku_sqare = event
-        window[event].update(button_color='grey')
-    elif isinstance(event, int):
-        if  1 <= event <= 9 and active_sudoku_sqare is not None:
-            model.get_board().insert_to_board(event, active_sudoku_sqare)
-            window[active_sudoku_sqare].update(event)
-            set_history(window, model)
-    elif isinstance(event, str):
-        if active_sudoku_sqare is not None and event == 'clear':
-            window[active_sudoku_sqare].update(button_color='#283b5b')
-            active_sudoku_sqare = None
-        if event == 'clear':
-            [[window[(row, col)].update('', disabled=False) for row in range(9)] for col in range(9)]
-            model.clear()
-            window['history'].update([])
-        if event == 'solve':
-            model.get_solver().set_patterns([values[key] for key in values.keys() if isinstance(key, int)])
-            model.solve()
-            [[window[(row, col)].update(model.get_board().get_board()[row, col]) for row in range(9) if model.get_board().get_board()[row, col] != 0] for col in range(9)]
-            set_history(window, model)
-        if event == 'folder':
-            if not model.load_file(values['folder']): sg.popup('Nem lehet betölteni a fájl!', title='Hiba!')
-            else:
-                window['sudoku_list'].update(list(model.get_board_keys()))
-        if event == 'sudoku_list':
-            if values['sudoku_list']:
-                model.load_board(values['sudoku_list'][0])
-                [[window[(row, col)].update('', disabled=False) for row in range(9)] for col in range(9)]
-                locked_fields = model.get_board_locked(values['sudoku_list'][0])
-                for row in range(9):
-                    for col in range(9):
-                        if model.get_board().get_board()[row, col] != 0:
-                            window[(row, col)].update(model.get_board().get_board()[row, col])
-                        if (row, col) in locked_fields:
-                            window[(row, col)].update(disabled=True)
-                set_history(window, model)
-        if event == 'back':
-            model.get_board().step_back_history()
-            print(model.get_board().get_board())
-            update_board(window, model)
-            set_history(window, model)
-        if event == 'forward':
-            model.get_board().step_forward_history()
-            [[window[(row, col)].update(model.get_board().get_board()[row, col]) for row in range(9) if model.get_board().get_board()[row, col] != 0] for col in range(9)]
-            set_history(window, model)
+    def __set_board_at(self, num, pos):
+        self.__window[pos].erase()
+        self.__window[pos].draw_text(num ,location=(18,18), font=('',15))
+
+    def __set_pv_at(self, nums, pos):
+        self.__window[pos].erase()
+        for num in nums:
+            self.__window[pos].draw_text(num, location=(9 * ((num-1)%3+1),36- 9 * ((num-1)//3+1)), font=('',8))
+
+    def __set_number(self, num, pos):
+        self.__set_board_at(num, pos)
+        self.__model.set_board_at(num, pos)
+
+    def __clear_board(self):
+        self.__model.clear()
+        self.__active_sq = None
+        for row in range(9):
+            for col in range(9):
+                self.__window[(row,col)].erase()
+                self.__window[(row,col)].update(self.__designs['inactive_color'])
+        self.__window['history'].update([])
+    
+    def __solve_board(self, values):
+        checkbox_values = [values[key] for key in values.keys() if isinstance(key, int)]
+        self.__model.set_pattern_names(checkbox_values)
+        his_pos = self.__model.get_his_pos()
+        self.__model.solve()
+        for row in range(9):
+            for col in range(9):
+                num = self.__model.get_board_at((row, col))
+                if num:
+                    self.__set_board_at(num, (row, col))
+        self.__set_history()
+    
+    def __fill_possible_values(self):
+        for row in range(9):
+            for col in range(9):
+                if self.__model.get_board_at((row, col)) == 0:
+                    self.__set_pv_at(np.argwhere(self.__model.get_pv_at(([0,1,2,3,4,5,6,7,8],row,col))).flatten()+1,(row, col))
+
+    def __get_file(self, path):
+        if not self.__model.load_file(path): sg.popup('Nem lehet betölteni a fájl!', title='Hiba!')
+        else:
+            self.__window['sudoku_list'].update(list(self.__model.get_keys()))
+
+    def __open_sudoku(self, selected_board):
+        self.__clear_board()
+        self.__model.load_board(selected_board)
+        for row in range(9):
+            for col in range(9):
+                if self.__model.get_board_at((row, col)) != 0:
+                    self.__set_board_at(self.__model.get_board_at((row, col)), (row, col))
+        self.__model.setup_possible_values()
+        self.__set_history()
+
+    def __set_history(self):
+        his = self.__model.get_history()
+        his_len = self.__model.get_his_pos()
+        self.__window['history'].update(his, set_to_index=[his_len])
+
+    def __back(self):
+        pass
+
+    def __forward(self):
+        self.__model.step_forward_history()
+        x,y, num = self.__model.get_history()[self.__model.get_his_pos()]
+        self.__set_board_at(num, (x,y))
+        self.__set_history()
 
 
-    print(event)
-    print(values)
-    #print(' ')
-    #print(window.key_dict)
+    def main(self):
+        while True:
+            event, values = self.__window.read()
+            if event in ('Exit', sg.WIN_CLOSED):
+                break
+            elif isinstance(event, tuple):
+                self.__set_active_sq(event)
+            elif isinstance(event, str):
+                if  event in ('1','2','3','4','5','6','7','8','9') and self.__active_sq is not None:
+                    self.__set_number(int(event), self.__active_sq)
+                    self.__set_history()
+                elif event == 'clear':
+                    self.__clear_board()
+                elif event == 'solve':
+                    self.__solve_board(values)
+                elif event == 'pen_marks':
+                    self.__fill_possible_values()
+                elif event == 'folder':
+                    self.__get_file(values['folder'])
+                elif event == 'sudoku_list':
+                    self.__open_sudoku(values['sudoku_list'][0])
+                elif event == 'back':
+                    self.__back()
+                elif event == 'forward':
+                    self.__forward()
+            
+            #print(values)
+
+
+if __name__ == '__main__':
+    window = Sudoku_Window()
+    window.main()
