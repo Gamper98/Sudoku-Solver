c4e5d7e21676f6753da4ae95f678bafee94a302c
Gamper98 <gampi1998@gmail.com>
2021-09-15 11:21:24 +0200

    Removed unused code, Rewritten solve()

--- Sudoku_Modell.py
+++ Sudoku_Modell.py
@@ -1,4 +1,3 @@
-#%%
 from Sudoku_Board import *
 from Sudoku_Solver import *
 import json
@@ -10,35 +9,67 @@ class Sudoku_Modell:
         self.__file = {}
         self.__active_board = None
 
+    #board methods    
     def get_board(self):
-        return self.__board
+        return self.__board.get_board().copy()
 
-    def get_solver(self):
-        return self.__solver
+    def get_board_at(self, pos):
+        return self.__board.get_board_at(pos)
 
-    def get_board_keys(self):
-        return self.__file.keys()
+    def get_pv_at(self, pos):
+        return self.__board.get_pv_at(pos)
     
-    def get_board_locked(self, key):
-        return self.__file[key]['locked']
+    def get_possible_values(self):
+        return self.__board.get_possible_values().copy()
+    
+    def get_history(self):
+        return self.__board.get_history()
 
-    def get_board_history(self, key):
-        return self.__file[key]['history']
+    def get_his_pos(self):
+        return self.__board.get_his_pos()
+
+    def set_board_at(self, num, pos):
+        self.__board.set_board_at(num, pos)
+
+    def append_to_history(self, his):
+        self.__board.append_to_history(his)
+
+    def step_back_history(self):
+        self.__board.step_back_history()
+
+    def step_forward_history(self): 
+        self.__board.step_forward_history()
+
+    #solver methods
+    def get_pattern_names(self):
+        return self.__solver.get_patterns_names()
+
+    def set_pattern_names(self, values):
+        self.__solver.set_pattern_names(values)
+
+    def setup_possible_values(self):
+        if np.all(self.__board.get_possible_values()):
+            self.__solver.setup_possible_values()
+
+    #model methods
+    def get_keys(self):
+        return self.__file.keys()
     
     def get_active_board(self):
         return self.__active_board
 
     def solve(self):
-        self.__solver.setup_possible_values()
-        solution = self.__solver.solve()
-        while solution is not None and np.any(solution.flatten()!=0) and np.any(self.__board.get_board().flatten()==0):
+        while True:
+            is_found, solution = self.__solver.solve()
+            if not is_found: return
+            if solution is None : continue
+            #if solution is None : return
             indeces = np.nonzero(solution)
             values = solution[indeces]
             self.__board.get_board()[indeces] = values
             [self.__board.append_to_history([index[0], index[1], value]) for index, value in zip(np.transpose(indeces), values)]
-            self.__solver.setup_possible_values()
-            solution = self.__solver.solve()
-        print(self.__board.get_history())
+            #return
+
     def clear(self):
         self.__board.clear()
         self.__active_board = None
@@ -50,7 +81,6 @@ class Sudoku_Modell:
             for key in temp.keys():
                 self.__file[key] = {}
                 self.__file[key]['board'] = np.array(temp[key]['board'], dtype=np.uint8)
-                self.__file[key]['locked']  = {(item[0], item[1]) for item in temp[key]['locked']}
                 self.__file[key]['history'] = temp[key]['history']
             f.close()
             return True
@@ -61,26 +91,3 @@ class Sudoku_Modell:
         self.__board.clear()
         self.__board.set_board(self.__file[key]['board'].copy(), self.__file[key]['history'].copy())
         self.__active_board = key
-        print(self.get_locked())
-        
-    def get_locked(self):
-        return np.argwhere(self.__file[self.__active_board]['board'])
-
-if __name__ == '__main__':
-
-
-    m = Sudoku_Modell()
-    board_2 = np.array([
-        [7,0,4,0,0,6,0,0,9],
-        [0,8,0,0,1,0,0,0,0],
-        [0,0,3,0,2,0,4,5,0],
-        [0,0,0,0,0,0,0,0,2],
-        [0,5,6,0,0,0,7,8,0],
-        [1,0,0,0,0,0,0,0,0],
-        [0,2,5,0,3,0,1,0,0],
-        [0,0,0,0,4,0,0,6,0],
-        [9,0,0,5,0,0,3,0,7]
-    ], dtype=np.int8)
-
-
-# %%
