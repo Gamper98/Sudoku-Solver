f3286404d08600bf9c4e3c7dab8dde1244798827
Gamper98 <gampi1998@gmail.com>
2021-11-04 11:11:44 +0100

    Added new History
    Added new methods and lines for Sudoku_History class
    Added step back/forward methods so it would change board based on History

--- Sudoku_Modell.py
+++ Sudoku_Modell.py
@@ -1,11 +1,14 @@
-from Sudoku_Board import *
-from Sudoku_Solver import *
+from Sudoku_Board import Sudoku_Board
+from Sudoku_Solver import Sudoku_Solver
+from Sudoku_History import *
+import numpy as np
 import json
 
 class Sudoku_Modell:
     def __init__(self):
-        self.__board = Sudoku_Board()
-        self.__solver = Sudoku_Solver(self.__board)
+        self.__history = Sudoku_History()
+        self.__board = Sudoku_Board(self.__history)
+        self.__solver = Sudoku_Solver(self.__board, self.__history)
         self.__file = {}
         self.__active_board = None
 
@@ -36,6 +39,13 @@ class Sudoku_Modell:
         if np.all(self.__board.get_possible_values()):
             self.__solver.setup_possible_values()
 
+    #history methods
+    def get_history(self):
+        return self.__history.get_history()
+
+    def get_his_pos(self):
+        return self.__history.get_his_pos()
+
     #model methods
     def get_keys(self):
         return self.__file.keys()
@@ -52,12 +62,12 @@ class Sudoku_Modell:
             indeces = np.nonzero(solution)
             values = solution[indeces]
             self.__board.get_board()[indeces] = values
-            [self.__board.append_to_history([index[0], index[1], value]) for index, value in zip(np.transpose(indeces), values)]
             #return
 
     def clear(self):
         self.__board.clear()
         self.__active_board = None
+        self.__history.reset()
 
     def load_file(self, path):
         try:
@@ -73,5 +83,21 @@ class Sudoku_Modell:
 
     def load_board(self, key):
         self.__board.clear()
-        self.__board.set_board(self.__file[key]['board'].copy(), self.__file[key]['history'].copy())
+        self.__board.set_board(self.__file[key]['board'].copy())
         self.__active_board = key
+
+    def step_back_his(self):
+        his = self.__history.step_back_his()
+        if his.op_type == Op_Type.Add:
+            self.__board.set_board_at(0, (his.row, his.col))
+        else:
+            self.__board.set_pv_at((his.num, his.row, his.col), True)
+        return his
+
+    def step_forward_his(self):
+        his = self.__history.step_forward_his()
+        if his.op_type == Op_Type.Add:
+            self.__board.set_board_at(his.num, (his.row, his.col))
+        else:
+            self.__board.set_pv_at((his.num, his.row, his.col), False)
+        return his
